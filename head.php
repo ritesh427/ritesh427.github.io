946a1b0df3f4a8034e660fd42c27d0dfd197c3da
Heisenberg <riteshpandey427@gmail.com>
2022-12-09 02:33:44 +0530

    Intial

---
+++ head.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Responsive Portfolio Website</title>
+    <!-- Link To CSS -->
+    <link rel="stylesheet" href="./css/style.css">
+    <!-- Box Icons -->
+    <link rel="stylesheet"
+  href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
+ 
+</head>
+<body>
+    <!-- Navbar -->
+    <header>
+        <a href="index1.php" class="logo">Bharti's Portfolio</a>
+ 
+        <div class="bx bx-menu" id="menu-icon"></div>
+ 
+        <ul class="navbar">
+            <li><a href="index1.php#home">Home</a></li>
+            <li><a href="index1.php#about">About</a></li>
+            <li><a href="index1.php#skills">Skills</a></li>
+            <li><a href="index1.php#services">Services</a></li>
+            <li><a href="index1.php#portfolio">Certificatios</a></li>
+            <li><a href="index.php" target="popup" >Let's Chat</a></li>
+            <div class="bx bx-moon" id="darkmode"></div>
+        </ul>
+    </header>
---
+++ index1.php
@@ -0,0 +1,161 @@
+<?php include_once "head.php"; ?>
+    <!-- Home -->
+    <section class="home" id="home">
+        <div class="social">
+            <a href="#"><i class='bx bxl-github'></i></a>
+            <a href="#"><i class='bx bxl-dribbble' ></i></a>
+            <a href="#"><i class='bx bxl-behance' ></i></a>
+        </div>
+        <div class="home-img">
+            <img src="11.png" alt="">
+        </div>
+        <div class="home-text">
+            <span>Hello, I'm</span>
+            <h1>Bharti Lekhak</h1>
+            <h2>Software Engineer</h2>
+            <p>a girl from village is here to show something I made.<br> Want to know about me?</p>
+            <a href="index.php" class="btn">Contact Me</a>
+        </div>
+ 
+    </section>
+    <!-- About -->
+    <section class="about" id="about">
+        <div class="heading">
+            <h2>About Me</h2>
+            <span>Introduction</span>
+        </div>
+        <!-- About Content -->
+        <div class="about-container">
+            <div class="about-img">
+                <img src="121.jpg" alt="">
+            </div>
+            <div class="about-text">
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Omnis beatae eius quae ab consequatur est perferendis dicta. Ad repellendus perferendis minus sunt error eos dicta enim accusantium ipsum veniam debitis quasi sint cum fugiat porro quam, blanditiis a ratione quod!</p>
+                <div class="information">
+                    <!-- Box 1 -->
+                    <div class="info-box">
+                        <i class='bx bxs-user' ></i>
+                        <span>Tasnia Farin</span>
+                    </div>
+                    <!-- Box 2 -->
+                    <div class="info-box">
+                        <i class='bx bxs-phone' ></i>
+                        <span>+1 444 444 4444</span>
+                    </div>
+                    <!-- Box 3 -->
+                    <div class="info-box">
+                        <i class='bx bxs-envelope' ></i>
+                        <span>project@farin.com</span>
+                    </div>
+                </div>
+                <a href="#" class="btn">Download Cv</a>
+            </div>
+        </div>
+    </section>
+    <!-- Skills -->
+    <section class="skills" id="skills">
+        <div class="heading">
+            <h2>Skills</h2>
+            <span>My Skills</span>
+        </div>
+        <!-- Skills Content -->
+        <div class="skills-container">
+            <div class="bars">
+                <!-- Box 1 -->
+                <div class="bars-box">
+                    <h3>HTML</h3>
+                    <span>94%</span>
+                    <div class="light-bar"></div>
+                    <div class="percent-bar html-bar"></div>
+                </div>
+                <!-- Box 2 -->
+                <div class="bars-box">
+                    <h3>CSS</h3>
+                    <span>84%</span>
+                    <div class="light-bar"></div>
+                    <div class="percent-bar css-bar"></div>
+                </div>
+                <!-- Box 3 -->
+                <div class="bars-box">
+                    <h3>JavaScript</h3>
+                    <span>74%</span>
+                    <div class="light-bar"></div>
+                    <div class="percent-bar js-bar"></div>
+                </div>
+                <!-- Box 4 -->
+                <div class="bars-box">
+                    <h3>React</h3>
+                    <span>80%</span>
+                    <div class="light-bar"></div>
+                    <div class="percent-bar react-bar"></div>
+                </div>
+            </div>
+            <div class="skills-img">
+                <img src="123.png" alt="">
+            </div>
+        </div>
+ 
+    </section>
+    <!-- Services -->
+    <section class="services" id="services">
+        <div class="heading">
+            <h2>Services</h2>
+            <span>Our Services</span>
+        </div>
+        <div class="services-content">
+            <!-- Box 1 -->
+            <div class="services-box">
+                <i class='bx bx-code-alt' ></i>
+                <h3>Web Development</h3>
+                <a href="#">Learn More</a>
+            </div>
+            <!-- Box 2 -->
+            <div class="services-box">
+                <i class='bx bx-server' ></i>
+                <h3>Backend Development</h3>
+                <a href="#">Learn More</a>
+            </div>
+            <!-- Box 3 -->
+            <div class="services-box">
+                <i class='bx bx-brush' ></i>
+                <h3>UI/UX Design</h3>
+                <a href="#">Learn More</a>
+            </div>
+
+            <div class="services-box">
+                <i class='bx bxl-wordpress' ></i>
+                <h3>Wordpress Developer</h3>
+                <a href="#">Learn More</a>
+            </div>
+        </div>
+    </section>
+    <!-- Portfolio -->
+    <section class="portfolio" id="portfolio">
+        <div class="heading">
+            <h2>Portfolio</h2>
+            <span>Our Recent Work</span>
+        </div>
+        <div class="portfolio-content">
+            <div class="portfolio-img">
+                <img src="blog-2.jpg" alt="">
+            </div>
+            <div class="portfolio-img">
+                <img src="blog-3.jpg" alt="">
+            </div>
+            <div class="portfolio-img">
+                <img src="blog-4.jpg" alt="">
+            </div>
+            <div class="portfolio-img">
+                <img src="blog-5.jpg" alt="">
+            </div>
+            <div class="portfolio-img">
+                <img src="blog-6.jpg" alt="">
+            </div>
+            <div class="portfolio-img">
+                <img src="blog-7.jpg" alt="">
+            </div>
+        </div>
+    </section>
+    <?php include_once "foot.php"; ?>
+</body>
+</html>
